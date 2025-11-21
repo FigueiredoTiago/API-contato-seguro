3570ec030549f2be6a668c9658ba7e358e98e0b9
TFscript <developertiagof@gmail.com>
2025-11-21 11:30:09 -0300

    Arquivo .ENV adc Propositalmente!

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import app from "./index";
 import dotenv from "dotenv";
 dotenv.config();
 import { connectDB } from "./dataBase/conection-db";
-const PORT = process.env.PORT || "5000";
+const PORT = process.env.PORT;
 
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT} /n Docs: http://localhost:${PORT}/api-docs`);
+  console.log(
+    `Server is running on port http://localhost:${PORT} /n Docs: http://localhost:${PORT}/api-docs`
+  );
 });
